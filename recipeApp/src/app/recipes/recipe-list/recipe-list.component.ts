a234a4e5e74c6ee1810c273295c025fb77a2fc49
itsca <itscae@gmail.com>
2017-06-14 17:35:43 -0600

    Finished section 3, app views and basic components and models

--- recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] = [
+		new Recipe("Test Recipe", "This is a test recipe", "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg")
+	];
 
   constructor() { }
 
