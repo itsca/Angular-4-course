89d1d5f3432ee78b212166c2d7a690f83a430189
itsca <itscae@gmail.com>
2017-06-18 20:21:01 -0600

    Added navigation with ng if instead of router

--- recipeApp/src/app/app.component.ts
+++ recipeApp/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  leadedFeature = 'recipe';
+  onNavigate(feature: string) {
+  	this.leadedFeature = feature;
+  }
 }
