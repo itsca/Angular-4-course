c5cbcd2e73d235f64298a5aaa6fbe523dd21b937
itsca <itscae@gmail.com>
2017-06-19 22:05:20 -0600

    added content child to server element

--- components-databinding/src/app/server-element/server-element.component.ts
+++ components-databinding/src/app/server-element/server-element.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, AfterContentInit, Input, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterContentInit {
+	@ContentChild('contentParagraph') paragraph: ElementRef;
+
 	@Input() element: {type: string, name: string, content: string};
 
 	constructor() { }
 
 	ngOnInit() {
+		console.log('Text of content paragraph on Init ' + this.paragraph.nativeElement.textContent)
+	}
+
+	ngAfterContentInit() {
+		console.log('Text of content paragraph on ContentInit ' + this.paragraph.nativeElement.textContent)
 	}
 
 }
