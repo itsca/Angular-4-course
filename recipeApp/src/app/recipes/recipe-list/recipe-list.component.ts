61c6005df8dd540cc868277023ff336cebec0053
itsca <itscae@gmail.com>
2017-06-19 23:35:48 -0600

    added event and property binding to comunicate current recipe to recipe details component and show current recipe

--- recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+	@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 	recipes: Recipe[] = [
-		new Recipe("Test Recipe", "This is a test recipe", "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg")
+		new Recipe("Test Recipe", "This is a test recipe", "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg"),
+		new Recipe("Test Recipe 2", "This is another test recipe", "http://media3.sailusfood.com/wp-content/uploads/2016/03/veg-momos-recipe.jpg")
 	];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+  	this.recipeWasSelected.emit(recipe);
   }
 
 }
