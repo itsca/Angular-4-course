203d8df1dd02a6e5d41734cecfaef6167f57b816
itsca <itscae@gmail.com>
2017-06-27 23:10:13 -0600

    added programatical navigation to new recipe and edit recipe

--- recipeApp/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipeApp/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { Ingredient } from '../../shared/ingredient.model';
 import { RecipesService } from '../recipes.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,7 +14,8 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipesService: RecipesService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -27,5 +28,10 @@ export class RecipeDetailComponent implements OnInit {
 
   addIngredients(ingredients: Ingredient[]) {
   	this.recipesService.addIngredients(ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    //this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route}); //More Complicated Path construction.
   }
 }
