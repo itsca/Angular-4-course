dcb0a1f31ebcdfae54f11606d1d6102217a86bfd
itsca <itscae@gmail.com>
2017-06-28 16:19:38 -0600

    created a simple observable and tested the 3 hooks

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+import 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-home',
@@ -10,6 +13,38 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	// const myNumbers = Observable.interval(1000);
+  	// myNumbers.subscribe(
+  	// 	(number : number) => {
+  	// 		console.log(number);
+  	// 	}
+  	// )
+
+  	const myObservable = Observable.create((observer: Observer<string>) => {
+  		setTimeout(() => {
+  			observer.next('First Package')
+  		}, 2000);
+  		setTimeout(() => {
+  			observer.next('Second Package')
+  		}, 4000);
+  		setTimeout(() => {
+  			//observer.error('This does not work')
+  		}, 5000);
+  		setTimeout(() => {
+  			observer.complete()
+  		}, 6000);
+  	});
+  	myObservable.subscribe(
+  		(data: string) => {
+  			console.log(data);
+  		},
+  		(error: string) => {
+  			console.log(error);
+  		},
+  		() => {
+  			console.log('completed')
+  		}
+  	)
   }
 
 }
