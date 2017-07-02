2fbe535a1861db22623bf79e6766e10317326ae3
itsca <itscae@gmail.com>
2017-07-01 18:02:20 -0600

    added default values with one way data binding

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') myForm : NgForm;
+  defaultQuestion = 'pet';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
