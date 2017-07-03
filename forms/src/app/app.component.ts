fc033f672309d3e4b7171e0c07afc339599f52ba
itsca <itscae@gmail.com>
2017-07-02 18:56:59 -0600

    added 2 way data binding and form groups

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') myForm : NgForm;
   defaultQuestion = 'pet';
+  answer = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
