a84d19ebef8a2c911822c2e4db178085cd56e3e9
itsca <itscae@gmail.com>
2017-07-02 19:23:56 -0600

    Patched form values and used form data to show summary on submit

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -10,12 +10,37 @@ export class AppComponent {
   @ViewChild('f') myForm : NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['male', 'female'];
+  user = {
+  	username: '',
+  	email: '',
+  	secretQuestion: '',
+  	answer: '',
+  	gender: '',
+  }
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    //this method is to set the whole forms value at once
+    // this.myForm.setValue({
+    // 	userData: {
+    // 		username: 'suggestedName',
+    // 		email: ''
+    // 	},
+    // 	secret: 'pet',
+    // 	questionAnswer: '',
+    // 	gender: 'male'
+    // })
+    this.myForm.form.patchValue({userData: {username: suggestedName}});
   }
 
   onSubmit(form: NgForm) {
-  	console.log(form);
+  	this.user.username = this.myForm.value.userData.username;
+  	this.user.email = this.myForm.value.userData.email;
+  	this.user.secretQuestion = this.myForm.value.secret;
+  	this.user.answer = this.myForm.value.questionAnswer;
+  	this.user.gender = this.myForm.value.gender;
+  	this.submitted = true;
   }
 }
